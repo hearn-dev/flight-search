3f2f9422986697364df3527626cc746a4189953a
Ethan Hearn <hearn.dev@gmail.com>
2023-01-13 10:40:22 -0500

    Function to auto populate City IATA codes

--- flight_search.py
+++ flight_search.py
@@ -1,4 +1,17 @@
+import requests
+import os
+
+TEQUILA_ENDPOINT = 'https://api.tequila.kiwi.com'
+TEQUILA_KEY = os.environ.get('TEQUILAKEY')
+
 class FlightSearch:
     def get_flight_code(self, city_name):
-        code = 'TESTING'
+        params = {
+            'term': city_name
+        }
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/locations/query",
+            headers = {"apikey": TEQUILA_KEY},
+            params = params)
+        code = response.json()['locations'][0]['code']
         return code
