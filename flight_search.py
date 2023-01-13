f053b215214edc8cacc10e2f94032da9bdddb4fc
Ethan Hearn <hearn.dev@gmail.com>
2023-01-13 11:51:24 -0500

    compares flights to data in google sheet using api

--- flight_search.py
+++ flight_search.py
@@ -1,13 +1,17 @@
 import requests
 import os
+from flight_data import FlightData
+from pprint import pprint
 
 TEQUILA_ENDPOINT = 'https://api.tequila.kiwi.com'
 TEQUILA_KEY = os.environ.get('TEQUILAKEY')
 
 class FlightSearch:
+
     def get_flight_code(self, city_name):
         params = {
-            'term': city_name
+            'term': city_name,
+            'location_types': 'city'
         }
         response = requests.get(
             url=f"{TEQUILA_ENDPOINT}/locations/query",
@@ -15,3 +19,43 @@ class FlightSearch:
             params = params)
         code = response.json()['locations'][0]['code']
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {'apikey': TEQUILA_KEY}
+        query = {
+            'fly_from': origin_city_code,
+            'fly_to': destination_city_code,
+            'date_from': from_time.strftime('%d/%m/%Y'),
+            'date_to': to_time.strftime('%d/%m/%Y'),
+            'nights_in_dst_from': 7,
+            'nights_in_dst_to': 28,
+            'flight_type': 'round',
+            'one_for_city': 1,
+            'max_stopovers': 0,
+            'curr': 'USD'
+        }
+
+        response = requests.get(
+            url=f'{TEQUILA_ENDPOINT}/v2/search',
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data=response.json()['data'][0]
+        except IndexError:
+            print(f'No flights found for {destination_city_code}')
+            return None
+
+        flight_data = FlightData(
+            price=data['price'],
+            origin_city=data['route'][0]['cityFrom'],
+            origin_airport=data['route'][0]['flyFrom'],
+            destination_city=data['route'][0]['cityTo'],
+            destination_airport=data['route'][0]['flyTo'],
+            out_date=data['route'][0]['local_departure'].split("T")[0],
+            return_date=data['route'][1]['local_departure'].split("T")[0],
+        )
+        print(f"{flight_data.destination_city}: ${flight_data.price}")
+        return flight_data
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from data_manager import DataManager
 from pprint import pprint
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
+STARTING_DESTINATION = 'NYC'
 dm = DataManager()
+flight_search = FlightSearch()
 sheet_data = dm.get_destination_data()
-print(pprint(sheet_data))
 
 if sheet_data[0]['iataCode'] == '':
     from flight_search import FlightSearch
@@ -15,3 +18,14 @@ if sheet_data[0]['iataCode'] == '':
 
     dm.destination_data = sheet_data
     dm.update_destination_codes() 
+
+tomorrow = datetime.now() +timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6*30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        STARTING_DESTINATION,
+        destination['iataCode'],
+        from_time=tomorrow,
+        to_time=six_months_from_today
+    )
