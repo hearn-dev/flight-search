0186b7fd41a982a5f7672018149a2260b6ce85e2
Ethan Hearn <hearn.dev@gmail.com>
2023-01-14 09:50:52 -0500

    Tequila API implemented to find city code

--- flight_search.py
+++ flight_search.py
@@ -1,10 +1,27 @@
+import os
+import requests
 
-
-# TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-# TEQUILA_API_KEY = os.environ.get('TEQUILAKEY')
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
+TEQUILA_API_KEY = os.environ.get('TEQUILAKEY')
 
 
 class FlightSearch:
     def find_codes(self, city):
-        code = "TESTING"
+        location_endpoint = f'{TEQUILA_ENDPOINT}/locations/query'
+        headers = {'apikey': '9nfQXGdg4UY-0iYLM1DdIxq5UH6ja0Tc'}
+        params = {
+            'term': city,
+            'location_types': 'city'
+            }
+        response = requests.get(
+            url=location_endpoint,
+            params=params,
+            headers=headers
+        )
+        
+        results = response.json()['locations']
+        code = results[0]['code']
         return code
+
+flight = FlightSearch()
+print(flight.find_codes('Knoxville'))
