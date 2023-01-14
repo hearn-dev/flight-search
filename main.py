398f2c6ec5f143f355336a0d4e73df442409777c
Ethan Hearn <hearn.dev@gmail.com>
2023-01-14 09:20:36 -0500

    functions to pull data from Sheet/update iata code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+data_manager = DataManager()
+
+sheet_data = data_manager.pull_data()['prices']
+
+if sheet_data[0]['iataCode'] =='':
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row['iataCode'] = flight_search.find_codes(row['city'])
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_codes()
